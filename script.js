c60213d2f34e450f2fef1800bbecff0d4e46f8dc
Vova <wishmaster2310@gmail.com>
2017-03-20 11:51:27 +0500

    up

--- script.js
+++ script.js
@@ -1,67 +1,60 @@
 var container = document.getElementById('container');
 var button = document.getElementById('addButton');
-button.onclick = Create; 
 
-function Create() {
-	var info 	 = document.getElementById('info');
-	var name     = document.getElementById('name');
-	var surname  = document.getElementById('surname');
-	var company  = document.getElementById('company');
-	this.name 	 = name.value;
-	this.surname = surname.value;
-	this.company = company.value;
-	if 
-	(name.value == false || surname.value == false || company.value == false ) {
-		info.innerHTML = "Недостаточно данных";
-		info.style.background = 'red';
-	} else {
-		var card = document.createElement('div');
-		card.className = "card";
-		var cardInner  = document.createElement('div');
-		card.innerHTML = 
-		"<p class='card__inner'> Имя: " + [this.name] + "</p>" + 
-		"<p class='card__inner'> Фамилия: " + [this.surname] + "</p>" + 
-		"<p class='card__inner'> Компания: " + [this.company] + "</p>";
-				var buttonDelete = document.createElement('button');
-				buttonDelete.className = "buttonDelete"; 
-				card.appendChild(buttonDelete); 
-		container.appendChild(card);
-			buttonDelete.onclick = function () {
-				this.parentNode.parentNode.removeChild(this.parentNode);
-			}
-		name.value = '';
-		surname.value = '';
-		company.value = '';
-		info.innerHTML = "Карточка создана";
-		info.style.background = 'green';
+
+
+function User(data) {
+	if (!data) { data = {}}
+	var self = this;
+	this.name = data.name;
+	this.id = Date.now();
+}
+
+var app = {
+	list: [],
+	addUser: function(data) {
+		var div, user, template;
+
+		user = new User(data)
+		app.list.push(user);
+		
+		template = [
+			'<div class="item__inner">',
+				'<div class="item__name">' + user.name + '</div>',
+			'</div>'
+		].join("");
+
+		div = document.createElement('div');
+		div.className = 'item';
+		div.setAttribute('data-id', user.id);
+		div.innerHTML = template;
+		container.appendChild(div);
+	},
+	removeUser: function(id) {
+
+	},
+	getList: function() {
+		return app.list
+	},
+	getItemById: function() {
+		// 
+	},
+	sortByName: function() {
+		// 
+	},
+	saveList: function() {
+		var list = [];
+		localStorage.setItem('list', list)
+	},
+	removeList: function() {
+		localStorage.removeItem('list')
+	},
+	createUserView: function(data) {
+
+	},
+	checkRequired: function() {
+		// qsa
 	}
 }
 
-
-
-
-
-
-function appear() {
-	var card = document.createElement('div');
-	card.className = "test";
-	card.innerHTML = 'wow';
-		// var button = document.createElement('button');
-		// button.className = "button1"
-		// button.innerHTML = 'Удалить запись';	 
-		// div.appendChild(button); 
-	container.appendChild(div);
-
-	// button.onclick = function () {
-	// 	this.parentNode.parentNode.removeChild(this.parentNode);
-	// }
-}
-
-
-
-// var user = new Card("name","Бабич", "jetStyle");
-// console.log(user.name);
-
-// var user1 = new Card("Елена","Мильчакова", "MTS");
-// console.log(user.name);
-// console.log(user1.surname);
+button.addEventListener('click', app.addUser);
